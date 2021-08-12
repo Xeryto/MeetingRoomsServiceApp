f4300b0b25a0533d498594e5b4ef939ea01beace
xeryto <igoshindanya@mail.ru>
2021-08-12 16:17:53 +0700

    initial commit

---
+++ MeetingRoomsServiceApp/Controllers/MeetingRoomsController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DataAccessLayer;
+using DataAccessLayer.Models;
+using BusinessLogic.Services;
+
+namespace MeetingRoomsServiceApp.Controllers
+{
+    public class MeetingRoomsController : Controller
+    {
+        private readonly MeetingRoomService _service;
+
+        public MeetingRoomsController(MeetingRoomService service)
+        {
+            _service = service;
+        }
+
+        // GET: MeetingRooms
+        public async Task<IActionResult> Index()
+        {
+            return View(await _service.GetAll());
+        }
+
+        // GET: MeetingRooms/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            return View(await _service.GetById(id));
+        }
+
+        // GET: MeetingRooms/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MeetingRooms/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] MeetingRoom meetingRoom)
+        {
+            if (ModelState.IsValid)
+            {
+                await _service.Add(meetingRoom);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(meetingRoom);
+        }
+
+        // GET: MeetingRooms/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            return View(await _service.GetById(id));
+        }
+
+        // POST: MeetingRooms/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] MeetingRoom meetingRoom)
+        {
+            if (ModelState.IsValid)
+            {
+                _service.Update(meetingRoom);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(meetingRoom);
+        }
+
+        // GET: MeetingRooms/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            return View(await _service.GetById(id));
+        }
+
+        // POST: MeetingRooms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _service.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ MeetingRoomsServiceApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MeetingRoomsServiceApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
